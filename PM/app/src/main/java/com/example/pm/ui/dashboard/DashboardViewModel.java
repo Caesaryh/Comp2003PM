db219f7ac3f09f6901ee6eb8d03668da9a9a4482
Tyler Bennett <tyler.bennett@students.plymouth.ac.uk>
2025-01-05 00:57:23 +0000

    screwed over by Android studio, will attempt after work tomorrow

---
+++ PM/app/src/main/java/com/example/pm/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package com.example.pm.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Password fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
